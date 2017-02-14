5a32273a677cba462a49f165fca03a393e37fbb4
OldFresco <hinsonkojo@gmail.com>
2017-02-14 02:15:29 +0000

    builds latest code

--- build/routes/index.js
+++ build/routes/index.js
@@ -1,7 +1,7 @@
 'use strict';
 
 Object.defineProperty(exports, "__esModule", {
-  value: true
+    value: true
 });
 
 var _meta = require('../http-request-handlers/meta.handler');
@@ -18,12 +18,47 @@ var _configuration = require('../configuration');
 
 var _configuration2 = _interopRequireDefault(_configuration);
 
+var _facebookMessengerBot = require('facebook-messenger-bot');
+
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+require('babel-polyfill');
+
+var bot = new _facebookMessengerBot.Bot(_configuration2.default.fb.myPageToken, _configuration2.default.fb.myVerification);
 
 var routes = new _express.Router();
 var version = _configuration2.default.apiVersion;
 
+bot.on('message', function _callee(message) {
+    var sender, out;
+    return regeneratorRuntime.async(function _callee$(_context) {
+        while (1) {
+            switch (_context.prev = _context.next) {
+                case 0:
+                    sender = message.sender;
+                    _context.next = 3;
+                    return regeneratorRuntime.awrap(sender.fetch('first_name'));
+
+                case 3:
+                    out = new _facebookMessengerBot.Elements();
+
+                    out.add({ text: 'hey ' + sender.first_name + ', how are you!' });
+
+                    _context.next = 7;
+                    return regeneratorRuntime.awrap(bot.send(sender.id, out));
+
+                case 7:
+                case 'end':
+                    return _context.stop();
+            }
+        }
+    }, null, undefined);
+});
+
+// Use fb bot framework middleware
+routes.use(version + '/webhook', bot.router());
 routes.get(version + '/meta', _meta2.default.index);
-routes.get(version + '/webhook', _webhook2.default.index);
+//routes.get(`${version}/webhook`, WebHookhandler.index);
+
 
 exports.default = routes;
--- build/server.js
+++ build/server.js
@@ -1,7 +1,7 @@
 'use strict';
 
 Object.defineProperty(exports, "__esModule", {
-    value: true
+  value: true
 });
 
 var _bodyParser = require('body-parser');
@@ -40,44 +40,11 @@ var _configuration = require('./configuration');
 
 var _configuration2 = _interopRequireDefault(_configuration);
 
-var _facebookMessengerBot = require('facebook-messenger-bot');
-
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 require('babel-polyfill');
 
-var bot = new _facebookMessengerBot.Bot(_configuration2.default.fb.myPageToken, _configuration2.default.fb.myVerification);
-
-bot.on('message', function _callee(message) {
-    var sender, out;
-    return regeneratorRuntime.async(function _callee$(_context) {
-        while (1) {
-            switch (_context.prev = _context.next) {
-                case 0:
-                    sender = message.sender;
-                    _context.next = 3;
-                    return regeneratorRuntime.awrap(sender.fetch('first_name'));
-
-                case 3:
-                    out = new _facebookMessengerBot.Elements();
-
-                    out.add({ text: 'hey ' + sender.first_name + ', how are you!' });
-
-                    _context.next = 7;
-                    return regeneratorRuntime.awrap(bot.send(sender.id, out));
-
-                case 7:
-                case 'end':
-                    return _context.stop();
-            }
-        }
-    }, null, undefined);
-});
-
 var app = (0, _express2.default)();
-
-// Use fb bot framework middleware
-app.use('/facebook', bot.router());
 
 // Adds some security best practices
 app.use((0, _helmet2.default)());
@@ -85,7 +52,7 @@ app.use((0, _cors2.default)());
 
 // Logger
 if (!_configuration2.default.envs.test) {
-    app.use((0, _morgan2.default)('dev'));
+  app.use((0, _morgan2.default)('dev'));
 }
 
 // Properly Decode JSON
@@ -100,12 +67,12 @@ app.use('/', _routes2.default);
 
 // Only use error handler in development
 if (_configuration2.default.envs.development) {
-    app.use((0, _errorhandler2.default)());
+  app.use((0, _errorhandler2.default)());
 }
 
 app.listen(_configuration2.default.port, function () {
-    // eslint-disable-next-line no-console
-    console.log('\n    === App Server ===\n\n    Connected on:\n    \n    Port: ' + _configuration2.default.port + '\n    Env: ' + app.get('env') + '\n    \n  ');
+  // eslint-disable-next-line no-console
+  console.log('\n    === App Server ===\n\n    Connected on:\n    \n    Port: ' + _configuration2.default.port + '\n    Env: ' + app.get('env') + '\n    \n  ');
 });
 
 exports.default = app;
