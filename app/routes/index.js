ab23858cbbc193c743282048f5d3c6560e21b427
OldFresco <hinsonkojo@gmail.com>
2017-02-14 01:48:51 +0000

    moves bot framework assets to router index

--- app/routes/index.js
+++ app/routes/index.js
@@ -2,11 +2,27 @@ import MetaHandler from '../http-request-handlers/meta.handler';
 import WebHookhandler from '../http-request-handlers/webhook.handler';
 import {Router} from 'express';
 import settings from '../configuration';
+import {Bot, Elements} from 'facebook-messenger-bot';
+require('babel-polyfill');
+
+const bot = new Bot(settings.fb.myPageToken, settings.fb.myVerification);
 
 const routes = new Router();
 const version = settings.apiVersion;
 
+// Use fb bot framework middleware
+routes.use('/facebook', bot.router());
 routes.get(`${version}/meta`, MetaHandler.index);
 routes.get(`${version}/webhook`, WebHookhandler.index);
+
+bot.on('message', async message => {
+    const {sender} = message;
+    await sender.fetch('first_name');
+
+    const out = new Elements();
+    out.add({text: `hey ${sender.first_name}, how are you!`});
+
+    await bot.send(sender.id, out);
+});
 
 export default routes;
